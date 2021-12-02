b2e062a6c77a1df244e9f2d46cdaa807b9657d95
Alex Chavkin <achavkin@scclin013.flatironinstitute.org>
2021-12-02 17:18:58 -0500

    add Day1

---
+++ Day1.hs
@@ -0,0 +1,22 @@
+module Day1 where
+
+nIncreases :: [Integer] -> Integer
+nIncreases [] = 0
+nIncreases (x:[]) = 0
+nIncreases (x:xs) = if x < head xs then 1 + next else next
+  where next = nIncreases xs
+
+n3Increases :: [Integer] -> Integer
+n3Increases l =  if length l < 4 then 0 else go l
+  where go (w:x:y:z:zs) = if w+x+y < x+y+z then 1 + next else next
+          where next = n3Increases (x:y:z:zs)
+
+getDistances :: IO [Integer]
+getDistances = do
+  distances <- readFile "distances.txt"
+  return $ (map (\x -> read x :: Integer) . lines $ distances)
+  
+main :: IO ()
+main = do
+  distances <- getDistances
+  print $ nIncreases distances; print $ n3Increases distances
